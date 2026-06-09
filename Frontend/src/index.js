e8105090eb3ce50275a7903d6fb7afb8b2446faf
JoAn-8 <U23243436@utp.edu.pe>
2026-06-09 16:09:10 -0500

    registrar rutas de reclamos en el servidor backend

--- Frontend/src/index.js
+++ Frontend/src/index.js
@@ -1,18 +1,24 @@
-import ReactDOM from "react-dom/client";
-import { BrowserRouter } from "react-router-dom";
-import App from "./App";
-import { CartProvider } from "./Context/CartContext";
-import { AuthProvider } from "./Context/AuthContext";
-import "bootstrap/dist/css/bootstrap.min.css";
-import "./App.css";
+import express from "express";
+import cors from "cors";
+import dotenv from "dotenv";
 
-const root = ReactDOM.createRoot(document.getElementById("root"));
-root.render(
-  <BrowserRouter>
-    <AuthProvider>
-      <CartProvider>
-        <App />
-      </CartProvider>
-    </AuthProvider>
-  </BrowserRouter>,
-);
+import productRoutes from "./routes/products.routes.js";
+import userRoutes from "./routes/user.routes.js";
+import cartRoutes from "./routes/cart.routes.js";
+import claimRoutes from "./routes/claim.routes.js";
+
+dotenv.config();
+
+const app = express();
+app.use(cors());
+app.use(express.json());
+
+app.use("/api/products", productRoutes);
+app.use("/api/users", userRoutes);
+app.use('/api/cart', cartRoutes);
+app.use("/api/claims", claimRoutes);
+
+const PORT = process.env.PORT || 5000;
+app.listen(PORT, () => {
+    console.log(`Servidor corriendo en http://localhost:${PORT}`);
+});
