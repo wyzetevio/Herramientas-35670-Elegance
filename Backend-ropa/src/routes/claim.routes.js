a524f1731552967f6b25f8c1e101dfec39ea286e
nohazyn <marionohaabal@gmail.com>
2026-06-10 02:17:38 -0500

    fix: permitir reclamos sin autenticarse (resolviendo el issue #12)

--- Backend-ropa/src/routes/claim.routes.js
+++ Backend-ropa/src/routes/claim.routes.js
@@ -1,9 +1,8 @@
 import { Router } from "express";
 import { createClaim } from "../controllers/claim.controller.js";
-import { verifyToken } from "../middleware/auth.js";
 
 const router = Router();
 
-router.post("/", verifyToken, createClaim);
+router.post("/", createClaim);
 
 export default router;
