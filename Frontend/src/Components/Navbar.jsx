e861bcead8e4a72699824fff9c5f4515dc043df5
JoAn-8 <U23243436@utp.edu.pe>
2026-06-21 16:09:51 -0500

    Update Navbar.jsx

--- Frontend/src/Components/Navbar.jsx
+++ Frontend/src/Components/Navbar.jsx
@@ -27,10 +27,6 @@ function NavigationBar() {
               Catálogo
             </Nav.Link>
 
-            <Nav.Link as={Link} to="/contact">
-              Contacto
-            </Nav.Link>
-
             <Nav.Link as={Link} to="/claims">
               Libro de Reclamaciones
             </Nav.Link>
